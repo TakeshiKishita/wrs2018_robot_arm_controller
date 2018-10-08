e355d045db153a10a74fdefcdc008eb29ef8bc62
TKC <xxtkcxx@gmail.com>
2018-10-08 16:08:18 +0900

    Revert "fix: 初期値からの増減をカンマ区切りで返すように変更"
    This reverts commit 0d1251937e005d497d6131e394f1359c5cd937e6.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 
 // アームの軸数
 const int axis_num = 6;
-int first_angle[6] = {};
 
 void setup() {
     Serial.begin( 9600 );
@@ -18,28 +17,16 @@ void loop() {
     // ントローラーの角度を読み取る
     String return_msg;
     for (size_t i = 0; i < axis_num; i++) {
-      int angle = analogRead(i);
-      if (first_angle[i] == 0) {
-        first_angle[i] = angle;
-        continue;
-      }
-
-      char temp_analog_in[4];
-      // 初期値からの増減を出力
-      sprintf(temp_analog_in, "%03d", angle-first_angle[i]);
+      char temp_analog_in[3] = "";
+      sprintf(temp_analog_in, "%03d", analogRead(i));
       String analog_in = String(temp_analog_in);
 
       // 送信するCSVを作成する
-      if (return_msg != "") {
-        analog_in = ","+analog_in;
-      }
       return_msg += analog_in;
     }
 
     // CSV形式のコントローラ角度を各関節に分解する
-    if (return_msg != "") {
-      Serial.println(return_msg);
-    }
+    Serial.println(return_msg);
 
     delay(100);
 }
